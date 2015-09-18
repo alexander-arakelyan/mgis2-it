49187d542eb8f85992bb8f253cdba05a8b654ead
Alexander Arakelyan <arakelyan@sovzond.ru>
2015-09-18 16:55:23 +0300

    - isogd specs.

--- run-protractor.sh
+++ run-protractor.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# npm install -g protractor
 # webdriver-manager start
+# sh -c 'while inotifywait -e close_write ./specs2/* ; do ./run-protractor.sh; done'
 
 protractor run-protractor-conf.js
+
---
+++ specs2/isogd-spec.js
@@ -0,0 +1,39 @@
+describe("isogd should work", function() {
+    var driver = browser.driver;
+    describe("sections", function() {
+	// TODO:
+	it("sections should be listed while clicking the menu", function() {
+	
+        });
+        it("create", function() {
+        });
+        it("read", function() {
+        });
+        it("update", function() {
+        });
+        it("delete", function() {
+        });
+    });
+
+    describe("books", function() {
+        it("first section click should direct to book list", function() {
+	});
+	it("create", function() {
+        });
+	it("read", function() {
+        });
+	it("update", function() {
+        });
+	it("delete", function() {
+        });
+    });
+
+    describe("volumes", function() {
+	// TODO:
+    });
+
+    describe("documents", function() {
+	// TODO:
+    });
+
+});
